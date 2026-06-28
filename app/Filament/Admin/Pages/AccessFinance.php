0c336fb43d94e434a2ba271ee88b98c65eb59acb
Joseph5157 <sikhajosephbhagyaraj@gmail.com>
2026-06-28 21:16:07 +0530

    feat: add Finance panel link to Admin navigation

---
+++ app/Filament/Admin/Pages/AccessFinance.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Admin\Pages;
+
+use Filament\Pages\Page;
+use Illuminate\Support\Facades\Redirect;
+
+class AccessFinance extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
+    protected static ?string $navigationLabel = 'Finance';
+    protected static ?int $navigationSort = 100;
+
+    public function mount()
+    {
+        return Redirect::to('/filament-finance');
+    }
+
+    public static function canAccess(): bool
+    {
+        return auth()->user()?->hasRole('admin') ?? false;
+    }
+}
